53e4271bc1dea4592577c56fa8c65f37e5688c31
LauraHexx <l.hesidenz97@gmail.com>
2025-05-27 21:29:42 +0200

    added: login start

--- videoflix/src/app/components/auth/home/home.component.ts
+++ videoflix/src/app/components/auth/home/home.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgStyle],
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss', '../auth-styles.css'],
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  backgroundImage = `
-    linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
-    url('/imgs/bg-home.jpg')
-  `;
-}
+export class HomeComponent {}
