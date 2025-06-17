35354b98321266cb8539941ca0f3e71d20fccf62
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-06-17 21:57:06 +0200

    add: main/components

---
+++ videoflix/src/app/features/main/componentes/media-home/media-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-home',
+  imports: [],
+  templateUrl: './media-home.component.html',
+  styleUrl: './media-home.component.scss'
+})
+export class MediaHomeComponent {
+
+}
---
+++ videoflix/src/app/features/main/componentes/video-carousel/video-carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-carousel',
+  imports: [],
+  templateUrl: './video-carousel.component.html',
+  styleUrl: './video-carousel.component.scss'
+})
+export class VideoCarouselComponent {
+
+}
