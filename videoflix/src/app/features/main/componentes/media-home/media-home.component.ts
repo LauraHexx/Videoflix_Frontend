f6e10acde6588d7fb7eea499ea34964cfd55491e
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-06-18 19:59:47 +0200

    feat: media-home

--- videoflix/src/app/features/main/componentes/media-home/media-home.component.ts
+++ videoflix/src/app/features/main/componentes/media-home/media-home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { CategoryGridComponent } from '../category-grid/category-grid.component';
 
 @Component({
   selector: 'app-media-home',
-  imports: [],
+  imports: [HeroSectionComponent, CategoryGridComponent],
   templateUrl: './media-home.component.html',
-  styleUrl: './media-home.component.scss'
+  styleUrl: './media-home.component.scss',
 })
 export class MediaHomeComponent {
+  topPickVideo = {
+    title: 'Top Pick Video',
+    description: 'This is a description of the top pick video',
+    image: 'https://via.placeholder.com/150',
+  };
 
+  categories = [];
 }
