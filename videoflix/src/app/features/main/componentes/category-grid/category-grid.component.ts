c77ac226ec197165158af18f6b1df601c6d4ad56
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-06-24 10:34:30 +0200

    style: media-home

--- videoflix/src/app/features/main/componentes/category-grid/category-grid.component.ts
+++ videoflix/src/app/features/main/componentes/category-grid/category-grid.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoCarouselComponent } from '../video-carousel/video-carousel.component';
-import { Video } from '../../../../shared/models/video';
-import { videos } from '../../../../../../public/dev_media/json_data';
+import { Video } from '@shared/models/video';
+import { videos } from '@public/dev_media/json_data';
 
 @Component({
   selector: 'app-category-grid',
@@ -11,7 +11,7 @@ import { videos } from '../../../../../../public/dev_media/json_data';
   styleUrl: './category-grid.component.scss',
 })
 export class CategoryGridComponent {
-  genres = ['nature', 'animals', 'cars', 'food', 'tech', 'sports'];
+  genres = ['nature', 'animals', 'sports'];
   videos: Video[] = videos;
   topPickVideo = videos[Math.floor(Math.random() * videos.length)];
 }
--- videoflix/src/app/features/main/componentes/hero-section/hero-section.component.ts
+++ videoflix/src/app/features/main/componentes/hero-section/hero-section.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Video } from '../../../../shared/models/video';
+import { videos } from '@public/dev_media/json_data';
 
 @Component({
   selector: 'app-hero-section',
   imports: [],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
+  @ViewChild('heroVideo') heroVideoRef!: ElementRef<HTMLVideoElement>;
 
+  videos: Video[] = videos;
+  topPickVideo = videos[Math.floor(Math.random() * videos.length)];
+
+  playVideo() {
+    if (this.heroVideoRef) {
+      const video = this.heroVideoRef.nativeElement;
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }
+  }
 }
