44638886d733ef083029afb120ce8890e9e18250
LauraHexx <l.hesidenz97@gmail.com>
2025-07-08 10:42:16 +0200

    added: continue watching videos

--- videoflix/src/app/features/main/componentes/video-carousel/video-carousel.component.ts
+++ videoflix/src/app/features/main/componentes/video-carousel/video-carousel.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoCardComponent } from '../video-card/video-card.component';
 import { Video } from '../../../../shared/models/video';
 import { UserWatchHistory } from '../../../../shared/models/user-watch-history';
 import { VideoService } from '../../services/video.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, catchError, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import {
   provideHttpClient,
@@ -21,14 +21,55 @@ import {
 export class VideoCarouselComponent {
   constructor(private videoService: VideoService, private http: HttpClient) {}
   @Input() genre: string = '';
+  @Input() isContinueWatching: boolean = false;
   videos$!: Observable<Video[]>;
-  watchHistory$!: Observable<UserWatchHistory[]>;
 
-  ngOnInit() {
-    this.videos$ = this.videoService.getAllVideos();
-    this.watchHistory$ = this.videoService
-      .getVideoWatchHistory()
-      .pipe(map((list) => list.filter((entry) => entry.progress > 0)));
-    console.log('videos$', this.videos$);
+  /**
+   * Lifecycle hook called when input properties change.
+   * Decides whether to load watch history videos or genre videos.
+   * @param changes - Object containing changes of input properties.
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.isContinueWatching) {
+      this.loadWatchHistoryVideos();
+    } else {
+      this.loadGenreVideos();
+    }
+  }
+
+  /**
+   * Loads videos from user's watch history where progress > 0.
+   */
+  private loadWatchHistoryVideos(): void {
+    this.videos$ = this.videoService.getVideoWatchHistory().pipe(
+      map((historyItems: UserWatchHistory[]) =>
+        historyItems
+          .filter((item) => item.progress > 0)
+          .map((item) => ({ ...item.video, watch_progress: item.progress }))
+      ),
+      catchError(() => of([]))
+    );
+  }
+
+  /**
+   * Loads all videos and filters by genre if specified.
+   */
+  private loadGenreVideos(): void {
+    this.videos$ = this.videoService.getAllVideos().pipe(
+      map((videos: Video[]) =>
+        this.genre ? videos.filter((v) => v.genre === this.genre) : videos
+      ),
+      catchError(() => of([]))
+    );
+  }
+
+  /**
+   * TrackBy function to optimize ngFor rendering by video id.
+   * @param index - The current index in the ngFor loop.
+   * @param video - The current video object.
+   * @returns The unique id of the video.
+   */
+  trackByVideoId(index: number, video: Video): number {
+    return video.id;
   }
 }
